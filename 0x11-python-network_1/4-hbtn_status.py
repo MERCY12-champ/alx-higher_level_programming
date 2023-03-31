5522c0ae24b8d383846550a22d1d3f623fb588a7
MERCY12-champ <wawiramercy066@gmail.com>
2023-03-31 08:36:18 -0700

    Python script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status."""
+"""
+Script that fetches https://intranet.hbtn.io/status.
+"""
+
 import requests
 
 
+def main():
+    """
+    Sends a GET request to https://intranet.hbtn.io/status and
+    displays the body of the response.
+    """
+    url = "https://intranet.hbtn.io/status"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        print("Body response:")
+        print("\t- type: {}".format(type(response.text)))
+        print("\t- content: {}".format(response.text))
+
+
 if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    main()
