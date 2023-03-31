45621a5199c1eef27237891e071002144d9308ff
MERCY12-champ <wawiramercy066@gmail.com>
2023-03-31 04:27:01 -0700

    A function that finds a peak in a list of unsorted integers

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,23 +1,33 @@
 #!/usr/bin/python3
-"""task 6 modele"""
 
+"""
+This module contains a function to find a peak in an unsorted list of integers.
+"""
 
 def find_peak(list_of_integers):
-    """Return a peak in a list of unsorted integers."""
-    if list_of_integers == []:
+    """
+    Finds a peak in an unsorted list of integers.
+
+    Args:
+        list_of_integers (list): List of integers to find a peak in.
+
+    Returns:
+        int: The peak value in the list.
+
+    Complexity:
+        O(log(n))
+    """
+    if not list_of_integers:
         return None
 
-    size = len(list_of_integers)
-    if size == 1:
-        return list_of_integers[0]
-    elif size == 2:
-        return max(list_of_integers)
+    low = 0
+    high = len(list_of_integers) - 1
 
-    mid = int(size / 2)
-    peak = list_of_integers[mid]
-    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
-        return peak
-    elif peak < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
-    else:
-        return find_peak(list_of_integers[mid + 1:])
+    while low < high:
+        mid = (low + high) // 2
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            low = mid + 1
+        else:
+            high = mid
+
+    return list_of_integers[low]
